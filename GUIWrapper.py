ebf5f60430ceb93cfcc7a2377f7a017bc76f791b
DaEvil1 <thomas.m.bolstad@gmail.com>
2016-07-08 23:08:52 +0200

    update

--- GUIWrapper.py
+++ GUIWrapper.py
@@ -1,10 +1,18 @@
 from _GUIWrapper import *
-import pygame, sys, os
+import sys, os
+import pygame
 from pygame.locals import *
 
 class Window(Window_):
 
+
     def __init__(self, name, width, height, position = (5, 20)):
+        
+        self.functions = {"background" : self._background,
+                          "transparent" : self._transparent,
+                          "fullscreen" : self._fullscreen,
+                          "save" : self._save}
+
         os.environ['SDL_VIDEO_WINDOW_POS'] \
                 = str(position[0]) + "," + str(position[1])
         pygame.init()
@@ -19,11 +27,6 @@ class Window(Window_):
                         "eventlist" : (0, 0), "width" : width, "height" : height,
                         "save" : None}
         
-        self.functions = {"background" : self._background,
-                          "transparent" : self._transparent,
-                          "fullscreen" : self._fullscreen,
-                          "save" : self._save}
-
         self.current = None
 
         #Needed to write text to canvas. Yay pygame...
@@ -49,6 +52,11 @@ class Window(Window_):
                 self.options["event"] = pygame.key.name(event.key)
             elif event.type == pygame.KEYUP:
                 self.options["event"] = pygame.key.name(event.key)
+            elif event.type == pygame.MOUSEBUTTONUP:
+                self.options["event"] = event.button, event.pos
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self.options["event"] = event.button, event.pos
+
 
     def Blit(self):
         self.options["screen"].fill(self.options["background"])
@@ -69,6 +77,179 @@ class Window(Window_):
     def Quit(self):
         pygame.quit()
         sys.exit()
+
+
+class Object(Object_):
+
+    def __init__(self, filename):
+        
+        self.functions = {"coords" : self._coords, "image" : self._image,
+                          "chop" : self._chop, "rotate" : self._rotate, 
+                          "scale" : self._scale, "transparent" : self._transparent}
+
+        image = pygame.image.load(filename)
+        rect = image.get_rect()
+        height, width = image.get_height(), image.get_width()
+        self.data = {"image" : image, "coords" : (0, 0), "rect" : rect,
+                     "chop" : None, "rotate" : 0, "height" : height,
+                     "width" : width, "scale" : (1, 1), "finalized" : False, 
+                     "rotated" : None, "transparent" : 0, "sub" : False}
+        
+
+        self.current = None
+
+    def PixelColor(self, coords):
+        return self.data["image"].get_at(coords)
+
+    def Modify(self, option, value):
+        self.current = value
+        self.data[option] = value
+        if option in self.functions:
+            self.functions[option]()
+        else:
+            raise Exception("given option not available.")
+
+    def Get(self, option):
+        return self.data[option]
+
+    def Remove(self):
+        self = None
+        
+    def Finalize(self):
+        self.data["finalized"] = True
+        if self.data["sub"]:
+            self.data["sub"] =pygame.Surface.convert(self.data["sub"])
+        else:
+            self.data["image"] =pygame.Surface.convert(self.data["image"])
+
+    def FinalizeAlpha(self):
+        self.data["finalized"] = True
+        if self.data["sub"]:
+            self.data["sub"] =pygame.Surface.convert_alpha(self.data["sub"])
+        else:
+            self.data["image"] =pygame.Surface.convert_alpha(self.data["image"])
+
+    def AlphaBlit(self, screen):
+        image = self.data["image"]
+        if self.data["sub"]:
+            image = self.data["sub"]
+        if self.data["rotated"]:
+            image = self.data["rotated"]
+            self.data["rotated"] = None
+        if not self.data["finalized"]:
+            image = pygame.Surface.convert_alpha(image)
+        screen.blit(image, self.data["rect"])
+
+
+    def Blit(self, screen):
+        image = self.data["image"]
+        if self.data["sub"]:
+            image = self.data["sub"]
+        if self.data["rotated"]:
+            image = self.data["rotated"]
+            self.data["rotated"] = None
+        if not self.data["finalized"]:
+            image = pygame.Surface.convert(image)
+        screen.blit(image, self.data["rect"])
+
+class Surface(Surface_):        
+
+    def __init__(self, width, height):
+
+        self.functions = {"coords" : self._coords, "chop" : self._chop, 
+                          "rotate" : self._rotate, "scale" : self._scale,
+                          "transparent" : self._transparent, 
+                          "save" : self._save, "d coords" : self._d_coords}
+
+
+        surface = pygame.Surface((width, height))
+        rect = surface.get_rect()
+        self.data = {"surface" : surface, "coords" : (0, 0), "rect" : rect,
+                     "chop" : None, "rotate" : 0, "height" : height,
+                     "width" : width, "scale" : (1, 1), "finalized" : False,
+                     "transparent" : 0}
+        
+
+        self.current = None
+
+    def PixelColor(self, coords):
+        return self.data["surface"].get_at(coords)
+
+    def Modify(self, option, value):
+        self.current = value
+        self.data[option] = value
+        if option in self.functions:
+            self.functions[option]()
+        else:
+            raise Exception("given option not available.")
+
+    def Remove(self):
+        self = None
+        
+    def Finalize(self):
+        self.data["finalized"] = True
+        self.data["surface"] =pygame.Surface.convert(self.data["surface"])
+
+    def FinalizeAlpha(self):
+        self.data["finalized"] = True
+        self.data["surface"] =pygame.Surface.convert_alpha(self.data["surface"])
+
+    def AlphaBlit(self, screen):
+        if not self.data["finalized"]:
+            self.data["surface"] =pygame.Surface.convert_alpha(self.data["surface"])
+        screen.blit(self.data["surface"], self.data["rect"])
+
+
+    def Blit(self, screen):
+        if not self.data["finalized"]:
+            self.data["surface"] = pygame.Surface.convert(self.data["surface"])
+        screen.blit(self.data["surface"], self.data["rect"])
+
+
+class Text(Text_):    
+
+    def __init__(self, string):
+
+        
+        self.functions = {"coords" : self._coords, "rotate" : self._rotate,
+                          "string" : self._string, "size" : self._size, 
+                          "font" : self._font, "color" : self._color,
+                          "outline" : self._outline}
+    
+
+        text = pygame.font.SysFont("Calibri", 12)
+        rend = text.render(string, True, (255, 255, 255))
+        rect = rend.get_rect()
+        outlines = []
+        for i in range(4):
+            outline = pygame.font.SysFont("Calibri", 12)
+            outlines.append(outline)
+        self.data = {"text" : text, "coords" : (0, 0), "rect" : rect,
+                     "rotate" : 0, "string" : string, "size" : 12, 
+                     "finalized" : False, "font" : "Calibri",
+                     "rendered" : rend, "color" : (255, 255, 255), 
+                     "outline" : None, "center" : False}
+        self.outline = {"text" : outlines, "rendered" : None}
+
+    def Modify(self, option, value):
+        self.current = value
+        self.data[option] = value
+        if option in self.functions:
+            self.functions[option]()
+        else:
+            raise Exception("given option not available.")
+
+    def Blit(self, screen):
+        obj = self.data["rendered"]
+        rect = self.data["rect"]
+        if self.data["outline"]:
+            x, y = rect.topleft
+            offset = ((-1, -1), (-1, 1), (1, -1), (1, 1))
+            for i in range(len(self.outline["rendered"])):
+                dx, dy = offset[i]
+                outline = self.outline["rendered"][i]
+                screen.blit(outline, (x + dx, y + dy))
+        screen.blit(obj, rect)
 
 class Objects:
 
@@ -92,6 +273,8 @@ class Objects:
     def Blit(self, screen):
         for element in self.ID:
             element.Blit(screen)
+
+
 
 
 #Does exactly the same as Objects. The only thing that needs to be fixed
--- _GUIWrapper.py
+++ _GUIWrapper.py
@@ -1,5 +1,6 @@
 #Includes the raw internal methods of GUI classes:
-import sys, pygame
+import sys
+import pygame
 
 class Window_:
 
@@ -8,7 +9,7 @@ class Window_:
         self.options["screen"].fill(self.current)
 
     def _transparent(self):
-        self.options["transparant"] = self.current
+        self.options["transparent"] = self.current
         self.options["screen"].set_colorkey(self.current)
 
     def _fullscreen(self):
@@ -24,78 +25,201 @@ class Window_:
 class Object_:
 
     def _coords(self):
-        self.data["rect"].x, self.data["rect"].y = self.current
+        x, y = self.current[0], self.current[1]
+        w, h = self.data["width"], self.data["height"]
+        self.data["rect"].topleft = (x , y)
+        #self.data["rect"] = (x, y, x + w, y + h)
+
+    def _transparent(self):
+        image = self.data["image"]
+        if self.data["sub"]:
+            image = self.data["sub"]
+        self.data["transparent"] = self.current
+        self.data["image"].set_colorkey(self.current)
+
 
     def _image(self):
         self.data["image"] = pygame.image.load(self.current)
+        self.data["sub"] = False
 
     def _chop(self):
-        self.data["image"] = self.data["image"].subsurface(self.current)
-        self.data["height"] = self.data["image"].get_height
-        self.data["width"] = self.data["image"].get_width
+        self.data["sub"] = self.data["image"].subsurface(self.current)
+        self.data["height"] = self.data["sub"].get_height()
+        self.data["width"] = self.data["sub"].get_width()
+        self.data["rect"] = self.data["sub"].get_rect()
+
+
+    def _rotate(self):
+        image = self.data["image"]
+        if self.data["sub"]:
+            image = self.data["sub"]
+        self.data["rotated"] = pygame.transform.rotozoom(image, self.current, 1)
+        self.data["rotate"] = 0
+        h, w = self.data["height"], self.data["width"]
+        rect = self.data["rotated"].get_rect()
+        dw, dh = rect[2] - w, rect[3] - h
+        chop = rect[0] + dw/2, rect[1] + dh/2, rect[2] - dw/2, rect[3] - dh/2
+        self.data["rotated"] = self.data["rotated"].subsurface(chop)
+
+    def _scale(self):
+        oldCenter = self.data["rect"].center
+        image = self.data["image"]
+        if self.data["sub"]:
+            image = self.data["sub"]
+        self.data["width"] *= self.data["scale"][0] 
+        self.data["height"] *= self.data["scale"][1]
+        self.data["width"] = int(round(self.data["width"]))
+        self.data["height"] = int(round(self.data["height"]))
+        w, h = self.data["width"], self.data["height"]
+        x, y = self.data["coords"][0], self.data["coords"][1]
+#        if w < 2  or h < 2:
+#            print(h, w)
+        self.data["image"] = pygame.transform.smoothscale(image, (w, h))
+        center = x + w/2.0, y + h/2.0
+        self.data["rect"].center = oldCenter
+
+
+class Surface_:
+
+    def _d_coords(self):
+        dx, dy = self.current[0], self.current[1]
+        x, y = self.data["rect"].topleft[0], self.data["rect"].topleft[1]
+        self.data["rect"].topleft = (x + dx, y + dy)
+
+    def _coords(self):
+        x, y = self.current[0], self.current[1]
+        w, h = self.data["width"], self.data["height"]
+        #center = x + w, y +  h
+        self.data["rect"].topleft = x, y
+        #self.data["rect"] = (x, y, x + w, y + h)
+
+    def _transparent(self):
+        self.data["transparent"] = self.current
+        self.data["surface"].set_colorkey(self.current)
+
+
+    def _chop(self):
+        self.data["surface"] = self.data["surface"].subsurface(self.current)
+        self.data["height"] = self.data["surface"].get_height()
+        self.data["width"] = self.data["surface"].get_width()
 
 
     def _rotate(self):
         oldCenter = self.data["rect"].center
-        self.data["image"] = pygame.transform.rotate(self.data["image"], self.current)
-        self.data["rect"] = self.data["image"].get_rect()
-        self.data["rect"] = self.data["image"].get_rect()
+        self.data["surface"] = pygame.transform.rotate(self.data["surface"], self.current)
+        self.data["rect"] = self.data["surface"].get_rect()
+        self.data["rect"] = self.data["surface"].get_rect()
         self.data["rect"].center = oldCenter
 
+    def _scale(self):
+        self.oldCenter = self.data["rect"].center
+        surface = self.data["surface"]
+        self.data["width"] *= self.data["scale"][0] 
+        self.data["height"] *= self.data["scale"][1]
+        self.data["width"] = int(round(self.data["width"]))
+        self.data["height"] = int(round(self.data["height"]))
+        w, h = self.data["width"], self.data["height"]
+        x, y = self.data["coords"][0], self.data["coords"][1]
+        self.data["surface"] = pygame.transform.smoothscale(surface, (w, h))
+        self.data["rect"].center = oldCenter
+        #self.data["rect"] = (x, y, x + w, y + h)
 
-class Object(Object_):
-        
-    def __init__(self, filename):
-
-        self.functions = {"coords" : self._coords, "image" : self._image,
-                          "chop" : self._chop, "rotate" : self._rotate}
-        
-
-        image = pygame.image.load(filename)
-        rect = image.get_rect()
-        height, width = image.get_height(), image.get_width()
-        self.data = {"image" : image, "coords" : (0, 0), "rect" : rect,
-                     "chop" : None, "rotate" : 0, "height" : height,
-                     "width" : width}
-        
-
-        self.current = None
-
-    def Transparent(self, color):
-        self.data["image"].set_colorkey(color)
-
-    def PixelColor(self, coords):
-        return self.data["image"].get_at(coords)
-
-    def Modify(self, option, value):
-        self.current = value
-        self.data[option] = value
-        if option in self.functions:
-            self.functions[option]()
-        else:
-            raise Exception("given option not available.")
-
-    def Get(self, option):
-        return self.data[option]
-
-    def Remove(self):
-        self = None
-
-    def AlphaBlit(self, screen):
-        self.data["image"] =pygame.Surface.convert_alpha(self.data["image"])
-        screen.blit(self.data["image"], self.data["rect"])
+    def _save(self):
+        pygame.image.save(self.data["surface"], self.current)
 
 
-    def Blit(self, screen):
-        self.data["image"] = pygame.Surface.convert(self.data["image"])
-        screen.blit(self.data["image"], self.data["rect"])
+class Text_:
+
+    def _rotate(self):
+        pass
+
+    def _coords(self):
+        x, y = self.current[0], self.current[1]
+        rect = self.data["rendered"].get_rect()
+        w, h = rect[2], rect[3]
+        #center = x + w, y +  h
+        rect.topleft = x, y
+        self.data["rect"] = rect
+        if self.data["center"]:
+            rect[0], rect[1] = x - w/2, y - h/2
+            self.data["rect"] = rect
+
+    def _string(self):
+        string = self.current
+        self.data["string"] = string
+        color = self.data["color"]
+        text = self.data["text"]
+        rend = text.render(string, True, color)
+        self.data["rendered"] = rend
+        if self.data["outline"]:
+            for i in range(len(self.outline["text"])):
+                rend = text.render(string, True, self.data["outline"])
+                self.outline["rendered"][i] = rend
+
+    def _color(self):
+        string = self.data["string"]
+        color = self.current
+        self.data["color"] = color
+        text = self.data["text"]
+        rend = text.render(string, True, color)
+        self.data["rendered"] = rend
+        if self.data["outline"]:
+            for i in range(len(self.outline["text"])):
+                rend = text.render(string, True, self.data["outline"])
+                self.outline["rendered"][i] = rend
+
+
+    def _font(self):
+        font = self.current
+        self.data["font"] = font
+        size = self.data["size"]
+        string = self.data["string"]
+        color = self.data["color"]
+        text = pygame.font.SysFont(font, size)
+        self.data["text"] = text
+        rend = text.render(string, True, color)
+        self.data["rendered"] = rend
+        if self.data["outline"]:
+            for i in range(len(self.outline["text"])):
+                text = pygame.font(SysFont(font, size))
+                rend = text.render(string, True, self.data["outline"])
+                self.outline["text"][i] = text
+                self.outline["rendered"][i] = rend
+
+    def _size(self):
+        size = self.current
+        self.data["size"] = size
+        font = self.data["font"]
+        string = self.data["string"]
+        color = self.data["color"]
+        text = pygame.font.SysFont(font, size)
+        self.data["text"] = text
+        rend = text.render(string, True, color)
+        self.data["rendered"] = rend
+        if self.data["outline"]:
+            for i in range(len(self.outline["text"])):
+                text = pygame.font(SysFont(font, size))
+                rend = text.render(string, True, self.data["outline"])
+                self.outline["text"][i] = text
+                self.outline["rendered"][i] = rend
+
+    def _outline(self):
+        outline = self.current
+        self.data["outline"] = outline
+        string = self.data["string"]
+        rendered = []
+        for i in self.outline["text"]:
+            rend = i.render(string, True, outline)
+            rendered.append(rend)
+        self.outline["rendered"] = rendered
 
 
 class Poly:
     
     options_order = \
         {
-        "line" : ["color", "start", "end", "width"]
+        "line" : ("color", "start", "end", "width"),
+        "polygon" : ("color", "points")
         }
     
 
@@ -103,9 +227,10 @@ class Poly:
         self.all_options = \
             {
                 "line" : {"start" : [0, 0], "end" : [0, 0], "color" : (0,0,0), \
-                          "width" : 0}
+                          "width" : 0},
+                "polygon" : {"color" : (0, 0, 0), "points" : ((0, 0), (0, 0), (0, 0))}
             }
-        self.draw = {"line" : pygame.draw.line}
+        self.draw = {"line" : pygame.draw.line, "polygon" : pygame.draw.polygon}
         if n_type not in self.draw:
             raise Exception("No such polygon available.")
         self.options = self.all_options[n_type]
@@ -114,35 +239,13 @@ class Poly:
     def Modify(self, option, value):
         self.options[option] = value
 
+    def Get(self, option):
+        return self.options[option]
+
     def Blit(self, screen):
         newargs = [self.all_options[self.type][i] for i in self.options_order[self.type]]
         self.draw[self.type](screen, *newargs)
         
-class Text:
-
-    def __init__(self, string, color, pos, size):
-        self.Font = pygame.font.SysFont("Arial", size)
-        self.obj = self.Font.render(string, False, color)
-        self.rect = self.obj.get_rect()
-        self.color = color
-        self.string = string
-        self.size = size
-        self.rect.x, self.rect.y = pos
-
-    def Pos(self, pos):
-        self.rect.x, self.rect.y = pos
-
-    def Color(self, color):
-        self.color = color
-
-    def Size(self, size):
-        self.size = size
-
-    def String(self, string):
-        self.string = string
-
-    def Blit(self, screen):
-        screen.blit(self.obj, self.rect) 
 
 ##class BuiltIn_:
 ##
